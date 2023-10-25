2637212a7023845c0d0237f31e721339af9881ca
Kovalski <andre.kovalski@tecpuc.com.br>
2023-10-25 14:01:07 -0300

    ex 9 OK

--- src/Main.hs
+++ src/Main.hs
@@ -122,6 +122,10 @@ ex8 n = fibo n [2, 1]
   -- 9. Escreva uma função análoga a função map, disponível no prelude, na qual um predicado, função,
   -- seja aplicado a duas listas e não a apenas uma. Por exemplo 𝑚𝑎𝑝2 (+) [1,2,3] [10,11,12] deve
   -- retornar [11,13,15]
+ex9 :: (Int -> Int -> Int) -> [Int] -> [Int] -> [Int]
+ex9 _ [] _ = []
+ex9 _ _ [] = []
+ex9 func param1 param2 = func(head param1) (head param2) : ex9 func (tail param1) (tail param2) 
 
 main::IO()
 main = do
@@ -132,3 +136,4 @@ main = do
   print (ex5 30)
   print (ex7 "as vogais foram trocadas!")
   print (ex8 150)
+  print (ex9 (+) [1, 2, 3] [10, 11, 12, 13, 14, 15])
